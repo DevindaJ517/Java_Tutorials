58f08015e63a076e15e563bc0c02e6fa17be5c44
Devinda Jayathilaka <168094003+DevindaJ517@users.noreply.github.com>
2025-10-25 14:36:11 +0530

    Add main.java file with a while loop example to demonstrate basic iteration

---
+++ While_loop/main.java
@@ -0,0 +1,10 @@
+
+public class main {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count < 4) {
+            System.out.println("Count is: " + count);
+            count++;
+        }
+    }
+}
